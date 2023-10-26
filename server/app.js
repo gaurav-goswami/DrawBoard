9058b1fff05d60bb0c57779f188fba27a7d370ad
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-10-26 16:56:34 +0530

    add error handler middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const errorMiddleware = require("./middlewares/errorMiddleware");
 
 dotenv.config({
     path : "./.env"
 })
 
 const app = express();
+
+app.use(errorMiddleware);
 
 module.exports = app;
